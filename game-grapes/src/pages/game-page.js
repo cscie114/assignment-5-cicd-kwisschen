ccae3b8bd83898956152602478d6ffb8e222e60a
Christopher Chen <chrischen0127@gmail.com>
2024-04-21 18:25:15 +0800

    Provide default value to try fixing the error during build

--- game-grapes/src/pages/game-page.js
+++ game-grapes/src/pages/game-page.js
@@ -1,36 +1,33 @@
 import React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import Layout from "../components/layout";
 
 const GamePage = ({ data }) => {
   const { game } = data;
 
-  if (!game) {
-    return (
-      <Layout>
-        <p>Game not found.</p>
-      </Layout>
-    );
-  }
-
   return (
     <Layout>
       <div>
-        <h1>{game.name}</h1>
-        {game.coverUrl && (
-          <img src={game.coverUrl} style={{ objectFit: 'contain', width: '20%', height: '20%', borderRadius: '10px' }} alt={`Cover image for ${game.name}`} />
+        {game && (
+          <>
+            <h1>{game.name}</h1>
+            {game.coverUrl && (
+              <img src={game.coverUrl} style={{ objectFit: 'contain', width: '20%', height: '20%', borderRadius: '10px' }} alt={`Cover image for ${game.name}`} />
+            )}
+            <h3>Rating:</h3>
+            <p>{game.rating ? game.rating.toFixed(1) : "N/A"}</p>
+            <h3>Summary:</h3>
+            <p>{game.summary}</p>
+          </>
         )}
-        <h3>Rating:</h3>
-        <p>{game.rating ? game.rating.toFixed(1) : "N/A"}</p>
-        <h3>Summary:</h3>
-        <p>{game.summary}</p>
+        {!game && <p>Game not found.</p>}
       </div>
     </Layout>
   );
 };
 
 export const query = graphql`
-  query GameQuery($gameId: String!) {
+  query GameQuery($gameId: String = "") {
     game(id: { eq: $gameId }) {
       id
       name
