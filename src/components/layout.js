764b0271be35fbf6c88fdd9c30fe34ce4a481f5f
Christopher Chen <chrischen0127@gmail.com>
2024-04-22 21:05:18 +0800

    Installed jest and eslint, pre-ran them locally

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import * as styles from "../styles/layout.module.css";
 
 const Layout = ({ children }) => (
@@ -22,9 +21,5 @@ const Layout = ({ children }) => (
     </footer>
   </div>
 );
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default Layout;
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,7 @@ const AboutPage = () => {
         <h1>About Game Grapes</h1>
         <p>Welcome to Game Grapes, your ultimate guide to the gaming world!</p>
         <p>At Game Grapes, we are passionate about gaming and strive to provide you with information on the highest-rated games of all time.</p>
-        <p>Our mission is to help you discover new and exciting games to play, whether you're a seasoned gamer or just getting started.</p>
+        <p>Our mission is to help you discover new and exciting games to play, whether you&apos;re a seasoned gamer or just getting started.</p>
         <p>Explore our website to find ratings, descriptions, and more. Happy gaming!</p>
       </div>
     </Layout>
